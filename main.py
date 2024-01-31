e85eb0e4640ccb6a7ea5fe5f105b39e7f78851b5
Nagesh Bathula <nageshbatthula@gmail.com>
2024-01-31 22:09:36 +0530

    added new code in pythons

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
 print("hello world")
+
+# prime number
+
+def is_prime(n):
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(2))
+
+# factorial
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+print(factorial(5))
